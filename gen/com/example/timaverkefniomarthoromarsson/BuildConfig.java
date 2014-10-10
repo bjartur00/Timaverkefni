f569d259eaecda30a22653f3d8a582fdca1bdcb9
bjartur00 <omar_tor@hotmail.com>
2014-10-10 13:31:23 +0000

    Fyrsta commit
    Tímaverkefni um git, fist commit

---
+++ gen/com/example/timaverkefniomarthoromarsson/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.timaverkefniomarthoromarsson;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
